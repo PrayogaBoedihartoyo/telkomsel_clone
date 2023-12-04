7bbeb12b50e79b3874844973283c1f744779a988
Prayoga Volantis <112841946+PrayogaBoedihartoyo28@users.noreply.github.com>
2023-12-04 13:41:55 +0700

    Initial Commit

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -8,7 +8,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     Widget headerContent(String icons, String text) {
       return Container(
-          margin: const EdgeInsets.only(top: 15 , right: 10),
+          margin: const EdgeInsets.only(top: 15, right: 10),
           padding: const EdgeInsets.fromLTRB(8, 5, 2, 5),
           decoration: BoxDecoration(
             color: darkRedColor,
@@ -80,6 +80,79 @@ class HomePage extends StatelessWidget {
       );
     }
 
+    Widget cardInfo() {
+      return Column(
+        children: [
+          Container(
+            height: 333,
+            margin: const EdgeInsets.only(top: 16, left: 16, right: 16),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15), color: whiteColor),
+            child: Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: greyCarColor,
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(15),
+                        topRight: Radius.circular(15),
+                      ),
+                    ),
+                    child: Padding(
+                      padding:
+                          const EdgeInsets.only(left: 17, top: 27, bottom: 21),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Rp 0',
+                              style: extraBoldText26.copyWith(
+                                  color: primaryTextColor)),
+                          Text('Active until 22 Sep 2023',
+                              style:
+                                  mediumText12.copyWith(color: darkGreyColor)),
+                          const Spacer(),
+                          Row(
+                            children: [
+                              Container(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 19, vertical: 7),
+                                decoration: BoxDecoration(
+                                  color: redButtonColor,
+                                  borderRadius: BorderRadius.circular(16.5),
+                                ),
+                                child: Text(
+                                  'Buy Package',
+                                  style:
+                                      mediumText12.copyWith(color: whiteColor),
+                                ),
+                              ),
+                              const SizedBox(width: 18),
+                              Text('Top Up',
+                                  style: mediumText12.copyWith(
+                                      color: darkGreyColor)),
+                              const SizedBox(width: 18),
+                              Text('Send Gift',
+                                  style: mediumText12.copyWith(
+                                      color: darkGreyColor)),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(),
+                )
+              ],
+            ),
+          ),
+        ],
+      );
+    }
+
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
@@ -97,6 +170,7 @@ class HomePage extends StatelessWidget {
         child: ListView(
           children: [
             header(),
+            cardInfo(),
           ],
         ),
       ),
