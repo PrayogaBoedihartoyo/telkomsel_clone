8dd1e1f008c45ecaf8f0f239f9d392cc1cad4ba1
Prayoga Volantis <112841946+PrayogaBoedihartoyo28@users.noreply.github.com>
2023-12-07 12:44:56 +0700

    Initial Commit

--- lib/screens/home/components/card_info_2.dart
+++ lib/screens/home/components/card_info_2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:telkomsel_clone/themes.dart';
 
 class CardInfoHome2 extends StatelessWidget {
@@ -6,11 +7,64 @@ class CardInfoHome2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget listCardInfo(String icons, IconData? iconHelp, String text, String text2, String text3) {
+      return Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Image.asset(icons, width: 36),
+          ),
+          Expanded(
+            flex: 10,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 11),
+              child: Column( //cok
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        text,
+                        style: boldText13.copyWith(color: primaryTextColor),
+                      ),
+                      Icon(iconHelp,
+                          color: primaryTextColor),
+                      const Spacer(),
+                      Text(
+                        text2,
+                        style: regulerText13.copyWith(color: primaryTextColor),
+                      ),
+                      Icon(Icons.chevron_right,
+                          color: primaryTextColor),
+                    ],
+                  ),
+                  Text(
+                    text3,
+                    style: regulerText10.copyWith(color: primaryTextColor),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+
     return Container(
-      margin: const EdgeInsets.only(top: 15, left: 15, right: 15),
-      height: 333,
+      margin: const EdgeInsets.only(top: 15, left: 16, right: 16),
+      padding: const EdgeInsets.only(left: 17, top: 29, right: 17),
       decoration: BoxDecoration(
-        color: whiteColor, borderRadius: BorderRadius.circular(15),
+        color: whiteColor,
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Column(
+        children: [
+          listCardInfo('assets/icons/phone.png', null, 'voice', 'You have no quota', 'Not yet purchased'),
+          const SizedBox(height: 35),
+          listCardInfo('assets/icons/SMS.png', null, 'SMS', 'You have no quota', 'Not yet purchased'),
+          const SizedBox(height: 35),
+          listCardInfo('assets/icons/Wallet.png', Icons.help_outline_rounded, 'Monetary', 'Not yet purchased', 'You have no monetary balance'),
+        ],
       ),
     );
   }
